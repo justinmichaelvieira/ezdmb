049af596f7088b7d905a8d051f6bbc112433ec96
Justin Vieira <justin.vieira@gmail.com>
2024-01-01 23:08:08 -0500

    Add initial debian install.sh

--- debian/install.sh
+++ debian/install.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 # Install script for debian targets.
 
+# install dependencies
+pip install -U pip
+pip install aqtinstall
+
+# install qt
+aqt install-qt linux desktop 5.15.12
+
+# Run debian package installer for the app
+sudo dpkg -i ezdmb_0.9.0_all.deb
