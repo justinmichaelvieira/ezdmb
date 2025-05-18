f5d1e6951d56437abdc8000ed71a498fb3dba56f
Justin Vieira <justin.vieira@gmail.com>
2025-05-17 22:01:54 -0400

    Remove unnecessary qt build tools from setup.py

--- setup.py
+++ setup.py
@@ -1,31 +1,11 @@
 from os.path import dirname, join, abspath
 from setuptools import setup, find_packages
 from setuptools.command.sdist import sdist
-from pyqt_distutils.build_ui import build_ui
 from src.ezdmb import version
 
 CURDIR = dirname(abspath(__file__))
 with open(join(CURDIR, "requirements.txt")) as rs:
     REQS = rs.read().splitlines()
-
-cmdclass = {}
-
-
-class custom_s_dist(sdist):
-    def run(self):
-        self.run_command('build_ui')
-        super().run()
-
-
-cmdclass["sdist"] = custom_s_dist
-
-
-class custom_build_ui(build_ui):
-    def run(self):
-        build_ui.run(self)
-
-
-cmdclass["build_ui"] = custom_build_ui
 
 with open(join(CURDIR, "README.md"), "r", encoding="utf-8") as fh:
     long_description = fh.read()
