bd8b72c03290131efa64572d8bc06f721bf6dde3
Justin Vieira <justin@rancorsoft.com>
2025-06-21 19:27:00 -0400

    Fix permissions on release upload step

--- ezdmb-installer.iss
+++ ezdmb-installer.iss
@@ -2,7 +2,7 @@
 
 [Setup] 
 AppName=ezdmb
-AppVerName=ezdmb v0.9.20
+AppVerName=ezdmb v0.9.21
 ChangesEnvironment=yes
 DefaultDirName={commonpf}\ezdmb
 DefaultGroupName=ezdmb
--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(join(CURDIR, "README.md"), "r", encoding="utf-8") as fh:
 
 setup(
     name='ezdmb',
-    version="0.9.20",
+    version="0.9.21",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/justinmichaelvieira/ezdmb",
