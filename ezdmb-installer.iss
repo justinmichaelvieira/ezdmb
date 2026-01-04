1b15e9a68eb90856e43b799c3f944976a43737a5
Justin Vieira <justin@rancorsoft.com>
2026-01-04 16:23:10 -0500

    Update ci test run

--- ezdmb-installer.iss
+++ ezdmb-installer.iss
@@ -2,7 +2,7 @@
 
 [Setup] 
 AppName=ezdmb
-AppVerName=ezdmb v0.9.44
+AppVerName=ezdmb v0.9.45
 ChangesEnvironment=yes
 DefaultDirName={commonpf}\ezdmb
 DefaultGroupName=ezdmb
--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(join(CURDIR, "README.md"), "r", encoding="utf-8") as fh:
 
 setup(
     name='ezdmb',
-    version="0.9.44",
+    version="0.9.45",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/justinmichaelvieira/ezdmb",
