e1c1b5217e7c74990768f7a9f44e4fd90ada544b
Justin Vieira <justin.vieira@gmail.com>
2026-04-19 17:07:20 -0400

    Fix paths in lin executable build

--- linux/debian/build-linux-executable.sh
+++ linux/debian/build-linux-executable.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-cd ../ezdmb || exit
+cd ../../ || exit
 
 # build the exe
-pyinstaller --onefile --name ezdmb --distpath ../src/ezdmb/dist ../src/ezdmb/__main__.py
+pyinstaller --onefile --name ezdmb --distpath src/ezdmb/dist src/ezdmb/__main__.py
 
 cd - || exit
 
 # copy the executable and stylesheet to the overlay folder
-mkdir -p overlay/opt/ezdmb
-cp ../src/ezdmb/dist/ezdmb overlay/opt/ezdmb/ezdmb
-cp ../style.css overlay/opt/ezdmb/style.css
+mkdir -p src/debian/overlay/opt/ezdmb
+cp ../../src/ezdmb/dist/__main__ ../../src/debian/overlay/opt/ezdmb/ezdmb
+cp ../../artifacts/style.css ../../src/debian/overlay/opt/ezdmb/style.css
