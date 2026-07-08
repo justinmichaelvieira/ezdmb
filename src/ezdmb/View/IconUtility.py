2a271b25fae9c0a585cf25923981e22481d9eaa4
Justin Vieira <justin@rancorsoft.com>
2026-07-08 01:05:00 -0400

    Fix flake8 warnings

--- src/ezdmb/View/IconUtility.py
+++ src/ezdmb/View/IconUtility.py
@@ -2,6 +2,7 @@ from PySide6.QtCore import QSize
 from PySide6.QtGui import QIcon
 import ezdmb.View.resources as resources  # noqa: F401, this is needed to load the resources.qrc file
 
+
 def getWindowIcon():
     icon = QIcon()
     icon.addFile(":/images/logo_48x48.png", QSize(48, 48))
