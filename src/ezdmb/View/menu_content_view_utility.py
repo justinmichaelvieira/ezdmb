090378fd53585ad9ef2bfb731e3809812d9e2370
Justin Vieira <justin@rancorsoft.com>
2026-09-05 18:44:16 -0400

    Update class ref, remove extraneous non working prev commit

--- src/ezdmb/View/menu_content_view_utility.py
+++ src/ezdmb/View/menu_content_view_utility.py
@@ -25,9 +25,6 @@ class menu_content_view_utility(QThread):
         self._config.configUpdated.connect(self.onConfigUpdated)
         self.start()
 
-    def __del__(self):
-        self.wait()
-
     @Slot(dict)
     def onConfigUpdated(self, data):
         self.contentArray = data["imported_content"]
